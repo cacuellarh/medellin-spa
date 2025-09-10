c241621801891a5b69e2c46239e0af01b06f8d41
camil <camilotedi@gmail.com>
2025-09-10 09:18:13 -0500

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { whatsappMsgDefault } from '../pages/planes/const';
-
 
 @Component({
   selector: 'app-root',
@@ -9,15 +9,21 @@ import { whatsappMsgDefault } from '../pages/planes/const';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'medellin-spa';
-  message : string = whatsappMsgDefault
+  message: string = whatsappMsgDefault;
 
-  menuToggle(){
-    const main = document.getElementById("menu")
-    const btn = document.getElementById("menu_btn")
-    main?.classList.toggle("hidden")
-    
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  menuToggle() {
+    if (isPlatformBrowser(this.platformId)) {
+      const main = document.getElementById("menu");
+      const btn = document.getElementById("menu_btn");
+      main?.classList.toggle("hidden");
+    }
   }
 
+  ngAfterViewInit() {
+    this.menuToggle();
+  }
 }
