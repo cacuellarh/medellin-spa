c1feffe99154700f8162811d8cce129a715109ae
camil <camilotedi@gmail.com>
2025-09-10 20:10:27 -0500

    change bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         primary: '#4C6B4A',
         primary_light: '#A0BA9E',
         primary_dark : '#022B04',
+        secondary_light: '#f8f4ee',
         secondary: '#CEAB5D',
         secondary_dark: '#9A7521',
         bg: '#F0FFEF'
