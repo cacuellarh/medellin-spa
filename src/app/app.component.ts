e4eed1e531e46c19d5a27ecc532112746d412af9
camil <camilotedi@gmail.com>
2025-09-10 21:30:54 -0500

    add main btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { whatsappMsgDefault } from '../pages/planes/const';
 import { WhatsappApiComponent } from './components/whatsapp-api/whatsapp-api.component';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, WhatsappApiComponent],
@@ -14,7 +14,7 @@ export class AppComponent {
   title = 'medellin-spa';
   message: string = whatsappMsgDefault;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private router: Router ) {}
 
   menuToggle() {
     if (isPlatformBrowser(this.platformId)) {
@@ -22,6 +22,10 @@ export class AppComponent {
       const btn = document.getElementById("menu_btn");
       main?.classList.toggle("hidden");
     }
+  }
+
+  toMain() {
+    this.router.navigate(['/']);
   }
 
 }
