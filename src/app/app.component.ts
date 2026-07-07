c7750e6f79307ce044be1d39203a3c3373a4dc63
Tu Nombre <tuemail@ejemplo.com>
2026-07-07 13:54:20 -0500

    popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { whatsappMsgDefault } from '../pages/planes/const';
 import { WhatsappApiComponent } from './components/whatsapp-api/whatsapp-api.component';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, WhatsappApiComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, WhatsappApiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'medellin-spa';
   message: string = whatsappMsgDefault;
+  showModal: boolean = true;
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object, private router: Router ) {}
 
@@ -26,6 +27,10 @@ export class AppComponent {
 
   toMain() {
     this.router.navigate(['/']);
+  }
+
+  closeModal() {
+    this.showModal = false;
   }
 
 }
